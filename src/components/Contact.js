24c8ac1b35e8b777a217cac7c965a23ca454ffa7
acatalina <accarrasco@gmail.com>
2017-03-18 16:30:11 +0000

    fix contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,21 +8,21 @@ var Contact = React.createClass({
       <section className="contact">
         <Form />
         <section className="contact-speedy">
-          <button className="contact-tel button" href="tel:07923953034">
+          <a className="contact-tel button" href="tel:07923953034">
             <span>call us now</span>
             <i className="fa fa-phone fa-2x" aria-hidden="true"></i>
             <span className="contact-tel-span">on 07923 953034</span>
-          </button>
-          <button className="contact-links button" 
+          </a>
+          <a className="contact-links button" 
             href="https://www.facebook.com/Speedy-Movers-Wigan-1646450355572387/">
             <span>follow us</span>
             <i className="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
-          </button>
-          <button className="contact-links button" 
+          </a>
+          <a className="contact-links button" 
             href="https://twitter.com/speedym2">
             <span>follow us</span>
             <i className="fa fa-twitter fa-2x" aria-hidden="true"></i>
-          </button>
+          </a>
         </section>
       </section>
     );
